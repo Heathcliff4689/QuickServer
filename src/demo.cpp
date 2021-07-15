3a4e47532619a0f33e40eceb0b83c90027bd6756
heathcliff <heathcliff4689@163.com>
2021-07-15 13:25:10 +0800

    Release

--- src/demo.cpp
+++ src/demo.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {   
     if(argc < 3)
     {
-        std::cout<<"Input the port & thread number you need, PLZ. ";
+        std::cout<<"Input the port & thread number you need, PLZ. \n";
         return 1;
     }
 
